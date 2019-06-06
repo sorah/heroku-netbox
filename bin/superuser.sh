e32f0c4a0d860226990156d456adc4e2bb779e9f
Sorah Fukumori <sorah@cookpad.com>
2019-06-06 13:11:29 +0900

    tmp

---
+++ bin/superuser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd "$(dirname $0)/../netbox"
+./manage.py shell --interface python << END
+from django.contrib.auth.models import User
+from users.models import Token
+if not User.objects.filter(username='${SUPERUSER_NAME}'):
+    u=User.objects.create_superuser('${SUPERUSER_NAME}', '${SUPERUSER_EMAIL}', '${SUPERUSER_PASSWORD}')
+    Token.objects.create(user=u, key='${SUPERUSER_API_TOKEN}')
+END
